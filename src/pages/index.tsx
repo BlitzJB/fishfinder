c3711c25b5d6b8652c8b91ff104114d4699e897f
BlitzJB <blitz04.dev@gmail.com>
2023-09-23 12:20:01 +0530

    feat: updated mock analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import React, { useState } from "react";
-import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, PieChart, Pie, Legend, Cell } from 'recharts';
 
 export default function Home() {
     const [showModal, setshowModal] = useState(false)
@@ -24,7 +24,7 @@ export default function Home() {
                     <button onClick={e => setshowModal(!showModal)}>Add Manually +</button>
                 </div>
             </div>
-            <div className="flex-grow">
+            <div className="flex-grow relative">
                 <div className="px-4 py-3 border border-neutral mt-4 flex items-center shad">
                     <div>
                         Home domains:
@@ -41,34 +41,33 @@ export default function Home() {
                 </div>
                 <div className="h-[25%] relative flex mt-6">
                     <div className="h-full w-[25%] mr-4 border border-neutral-300 shadow-lg rounded-md flex flex-col items-center justify-center">
-                        <div className="font-bold text-2xl mb-3">Metric</div>
-                        <div className="text-5xl">Value</div>
+                        <div className="font-bold text-2xl mb-3">Avg URL Similarity</div>
+                        <div className="text-5xl">93.57%</div>
                     </div>
                     <div className="h-full w-[25%] mr-4 border border-neutral-300 shadow-lg rounded-md flex flex-col items-center justify-center">
-                        <div className="font-bold text-2xl mb-3">Metric</div>
-                        <div className="text-5xl">Value</div>
+                        <div className="font-bold text-2xl mb-3">Text Filter Hits</div>
+                        <div className="text-5xl">95.30%</div>
                     </div>
                     <div className="h-full w-[25%] mr-4 border border-neutral-300 shadow-lg rounded-md flex flex-col items-center justify-center">
-                        <div className="font-bold text-2xl mb-3">Metric</div>
-                        <div className="text-5xl">Value</div>
+                        <div className="font-bold text-2xl mb-3">Domain Info Hits</div>
+                        <div className="text-5xl">96.78%</div>
                     </div>
                     <div className="h-full w-[25%] border border-neutral-300 shadow-lg rounded-md flex flex-col items-center justify-center">
-                        <div className="font-bold text-2xl mb-3">Metric</div>
-                        <div className="text-5xl">Value</div>
+                        <div className="font-bold text-2xl mb-3">AI Filter Hits</div>
+                        <div className="text-5xl">98.22%</div>
                     </div>
                 </div>
                 <div className="h-[60%] relative flex flex-col">
                     <div>
-                        <h1 className="text-2xl font-bold mt-4 mb-2 text-neutral-500">Recent Activity</h1>
+                        <h1 className="text-2xl font-bold mt-4 mb-2 text-neutral-700">Detections by Day</h1>
                     </div>
                     <div className="w-full flex-grow flex">
                         <div className="w-[75%] h-full border border-neutral-200 rounded-md shadow-xl mr-4 flex items-center justify-center pr-9">
-                            <Visualization />
+                            <LineChartVisualization />
                         </div>
                         <div className="w-[25%] relative">
-                            <div className="h-full mb-4 w-full border border-neutral-200 shadow-lg rounded-md flex flex-col items-center justify-center">
-                                <div className="font-bold text-2xl mb-3">Metric</div>
-                                <div className="text-5xl">Value</div>
+                            <div className="h-full mb-4 w-full border border-neutral-200 shadow-lg rounded-md flex flex-col items-center justify-center overflow-hidden">
+                                <PieChartVisualization />
                             </div>
                         </div>
                     </div>
@@ -96,7 +95,7 @@ export const Layout = ({ children }: LayoutProps) => {
                     <ActionIcon src="/icons/list.svg" url="/list" />
                 </div>
             </nav>
-            <main className="px-8 py-8 flex flex-col flex-grow">
+            <main className="px-8 py-8 flex flex-col flex-grow w-[calc(100vw-64px)]">
                 {children}
             </main>
         </div>
@@ -148,19 +147,48 @@ const Modal = ({ setShowModal, setModalValue, modalValue }: ModalProps) => {
     </div>
 }
 
-const Visualization = () => {
+const LineChartVisualization = () => {
     const data = [
-        { name: '16-9-23', uv: 400, pv: 2400, amt: 2400 }, 
-        { name: '17-9-23', uv: 500, pv: 2400, amt: 2400 }, 
-        { name: '18-9-23', uv: 300, pv: 2400, amt: 2400 }, 
-        { name: '19-9-23', uv: 700, pv: 2400, amt: 2400 },
-        { name: '20-9-23', uv: 700, pv: 2400, amt: 2400 },
-        { name: 'Yesterday', uv: 700, pv: 2400, amt: 2400 },
-        { name: 'Today', uv: 700, pv: 2400, amt: 2400 },
+        { name: '16-9-23', PhishingDomains: 400, pv: 2400, amt: 2400 }, 
+        { name: '17-9-23', PhishingDomains: 500, pv: 2400, amt: 2400 }, 
+        { name: '18-9-23', PhishingDomains: 300, pv: 2400, amt: 2400 }, 
+        { name: '19-9-23', PhishingDomains: 700, pv: 2400, amt: 2400 },
+        { name: '20-9-23', PhishingDomains: 800, pv: 2400, amt: 2400 },
+        { name: 'Yesterday', PhishingDomains: 750, pv: 2400, amt: 2400 },
+        { name: 'Today', PhishingDomains: 720, pv: 2400, amt: 2400 },
     ]
     return <LineChart width={1000} height={300} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="PhishingDomains" stroke="#8884d8" />
         <XAxis dataKey="name" />
         <YAxis />
+        <Tooltip />
     </LineChart>
+}
+
+const PieChartVisualization = () => {
+    const data = [
+        { name: 'discord.com', value: 400, color: "#8884d8" }, 
+        { name: 'steamcommunity.com', value: 600, color: "#82ca9d" }, 
+    ]
+    const [isClient, setIsClient] = useState(false)
+ 
+    React.useEffect(() => {
+        setIsClient(true)
+    }, [])
+
+    return <>
+        {
+            isClient ? <PieChart width={300} height={300}>
+                <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
+                    {
+                        data.map((entry, index) => <Cell key={`cell-${index}`} fill={entry.color} />)
+                    }
+                </Pie>
+                <Tooltip />
+                <Legend />
+            </PieChart> : null
+        }    
+    </> 
+    
+    
 }
